68a9aad5ed0165909559bb31dae767775520feae
Дэннис Арабалеев <ghostamane@mail.ru>
2023-02-27 21:33:17 +0400

    Изменение свойств, на авто-свойства, путем PropertyChanged.Fody(NuGet)

--- CV19/ViewModels/MainWindowViewModel.cs
+++ CV19/ViewModels/MainWindowViewModel.cs
@@ -6,25 +6,11 @@ namespace CV19.ViewModels
     {
         #region Заголовок окна
 
-        #endregion
-        private string _Title = "Анализ статистики CV19";
-
         /// <summary>
         /// Заголовок окна
         /// </summary>
-        public string Title
-        {            
-            get => _Title;
+        public string Title { get; set; } = "Анализ статистики CV19";
 
-            //set 
-            //{
-            //    if (Equals(Title, value)) return;
-            //    _Title = value;
-            //    OnPropertyChanged();
-
-            //    Set(ref _Title,value)
-            //}
-            set => Set(ref _Title, value);
-        }
+        #endregion
     }
 }
