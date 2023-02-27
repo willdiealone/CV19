95e4455b2a0b1f9083f1074f76bea611e710ba49
Дэннис Арабалеев <ghostamane@mail.ru>
2023-02-27 19:32:37 +0400

    Тестирование работы модели преставления(View)

--- CV19/ViewModels/MainWindowViewModel.cs
+++ CV19/ViewModels/MainWindowViewModel.cs
@@ -1,12 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using CV19.ViewModels.Base;
 
 namespace CV19.ViewModels
 {
-    class MainWindowViewModel
+    internal class MainWindowViewModel : BaseViewModel
     {
+        #region Заголовок окна
+
+        #endregion
+        private string _Title = "Анализ статистики CV19";
+
+        /// <summary>
+        /// Заголовок окна
+        /// </summary>
+        public string Title
+        {            
+            get => _Title;
+
+            //set 
+            //{
+            //    if (Equals(Title, value)) return;
+            //    _Title = value;
+            //    OnPropertyChanged();
+
+            //    Set(ref _Title,value)
+            //}
+            set => Set(ref _Title, value);
+        }
     }
 }
